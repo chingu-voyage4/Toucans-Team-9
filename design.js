0d2a4f38e1902ba79cd789d4250abd7eaab98a83
catarina Veiga <cveiga819gmail.com>
2018-03-25 14:02:28 +0100

    add the brands_slide

--- design.js
+++ design.js
@@ -1,1 +1,16 @@
-// js file
+// brands slideshow 
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("brands_slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
